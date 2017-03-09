8473390c39e22258218a3edcddb916ac38c04119
Jon Ander Hernandez <jonan.h@gmail.com>
2017-03-09 09:06:09 +0200

    Add new TypeInfo and TypeMember classes

---
+++ Elurnity.Serialization/TypeInfo/TypeInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Elurnity.Serialization
+{
+    public class TypeInfo
+    {
+        public readonly string Name;
+
+        public readonly Action<object> Constructor;
+
+        public readonly TypeMember[] members;
+    }
+
+    public partial class TypeInfo<T> : TypeInfo where T : class
+    {
+        public readonly Action<T> Constructor2;
+    }
+}
---
+++ Elurnity.Serialization/TypeInfo/TypeMember.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Elurnity.Serialization
+{
+    public abstract partial class TypeMember
+    {
+        public string Name;
+        public Type type;
+
+        // Generic getter and setter with boxing/unboxing
+
+        public abstract object Get(object instance);
+        public abstract object Set(object instance, object field);
+    }
+
+    public abstract partial class TypeMember<T>
+    {
+    }
+
+    public partial class ValueTypeMember<T, U> : TypeMember<T> where T : struct
+    {
+        public delegate U GetterDelegate(ref T instance);
+        public delegate void SetterDelegate(ref T instance, U value);
+
+        public GetterDelegate Getter;
+        public SetterDelegate Setter;
+
+        public override object Get(object instance)
+        {
+            var i = (T)instance;
+            return Getter(ref i);
+        }
+
+        public override object Set(object instance, object field)
+        {
+            var i = (T)instance;
+            Setter(ref i, (U)field);
+            return i;
+        }
+    }
+
+    public partial class TypeMember<T, U> : TypeMember<T> where T : class
+    {
+        public delegate U GetterDelegate(T instance);
+        public delegate void SetterDelegate(T instance, U field);
+
+        public GetterDelegate Getter;
+        public SetterDelegate Setter;
+
+        public override object Get(object instance)
+        {
+            return Getter((T)instance);
+        }
+
+        public override object Set(object instance, object field)
+        {
+            var i = (T)instance;
+            Setter(i, (U)field);
+            return i;
+        }
+    }
+}
