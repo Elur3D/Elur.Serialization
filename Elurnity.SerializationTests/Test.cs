9174319996a9bdba387c84609934bdab71306fe5
Jon Ander Hernandez <jonan.h@gmail.com>
2017-03-09 09:11:58 +0200

    Add some very basic tests

---
+++ Elurnity.SerializationTests/Test.cs
@@ -0,0 +1,81 @@
+﻿
+using NUnit.Framework;
+
+namespace Elurnity.Serialization.Tests
+{
+    [TestFixture]
+    public class TestSerialization
+    {
+        public class ClassA
+        {
+            public int intField;
+            public int intProperty { get; set; }
+        }
+
+        public struct StructA
+        {
+            public int intField;
+            public int intProperty { get; set; }
+        }
+
+        [Test]
+        public void TestWrapClass()
+        {
+            var wrap = TypeWrap.Wrap(new ClassA
+            {
+                intField = 100,
+                intProperty = 200,
+            });
+
+            Assert.AreEqual(100, wrap["intField"]);
+            Assert.AreEqual(200, wrap["intProperty"]);
+        }
+
+        [Test]
+        public void TestUnwrapClass()
+        {
+            var wrap = new TypeWrap()
+            {
+                ["intField"] = 100,
+                ["intProperty"] = 200,
+            };
+
+            var instance = System.Activator.CreateInstance(typeof(ClassA)) as ClassA;
+
+            wrap.Unwrap(instance);
+
+            Assert.AreEqual(100, instance.intField);
+            Assert.AreEqual(200, instance.intProperty);
+        }
+
+        [Test]
+        public void TestWrapStruct()
+        {
+            var wrap = TypeWrap.Wrap(new StructA
+            {
+                intField = 100,
+                intProperty = 200,
+            });
+
+            Assert.AreEqual(100, wrap["intField"]);
+            Assert.AreEqual(200, wrap["intProperty"]);
+        }
+
+        [Test]
+        public void TestUnwrapStruct()
+        {
+            var wrap = new TypeWrap()
+            {
+                ["intField"] = 100,
+                ["intProperty"] = 200,
+            };
+
+            var instance = (StructA)System.Activator.CreateInstance(typeof(StructA));
+
+            wrap.Unwrap(instance);
+
+            Assert.AreEqual(100, instance.intField);
+            Assert.AreEqual(200, instance.intProperty);
+        }
+    }
+}
---
+++ Elurnity.SerializationTests/Test2.cs
@@ -0,0 +1,42 @@
+﻿
+using NUnit.Framework;
+
+namespace Elurnity.Serialization.Tests
+{
+    [TestFixture]
+    public class TestSerialization2
+    {
+        public class ClassA
+        {
+            public int intField;
+            public int intProperty { get; set; }
+        }
+
+        [Test]
+        public void TestWrapClass()
+        {
+            var info = TypeInfoReflection.getInfo(typeof(ClassA));
+
+            Assert.AreEqual(info.members.Count, 2);
+
+            var classA = new ClassA
+            {
+                intField = 50,
+                intProperty = 100,
+            };
+
+            Assert.AreEqual(info.members[0].Get(classA), 100);
+
+            info.members[0].Set(classA, 200);
+
+            Assert.AreEqual(info.members[0].Get(classA), 200);
+
+
+            Assert.AreEqual(info.members[1].Get(classA), 50);
+
+            info.members[1].Set(classA, 200);
+
+            Assert.AreEqual(info.members[1].Get(classA), 200);
+        }
+    }
+}
