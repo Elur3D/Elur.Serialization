30010c6eceb3073a8cf2b9da0e97f0cc891c22d5
Jon Ander Hernandez <jonan.h@gmail.com>
2017-03-09 09:10:08 +0200

    Add a TypeInfo implementation using open delegates constructed with reflection

---
+++ Elurnity.Serialization/FastReflection/TypeInfoReflection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Elurnity.Serialization
+{
+    public class TypeInfoReflection
+    {
+        public List<TypeMember> members;
+
+        protected TypeInfoReflection()
+        {
+        }
+
+        public static TypeInfoReflection getInfo(Type type)
+        {
+            List<TypeMember> members = new List<TypeMember>();
+
+            foreach (PropertyInfo info in type.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                var _info = info;
+
+                MethodInfo getMethodInfo = null;
+                MethodInfo setMethodInfo = null;
+
+                if (info.CanRead && info.GetIndexParameters().Length == 0)
+                {
+                    getMethodInfo = _info.GetGetMethod();
+                }
+
+                if (info.CanWrite)
+                {
+                    setMethodInfo = _info.GetSetMethod();
+                }
+
+                var memberTypeInfo = ReflectionMember.CreateMember(getMethodInfo, setMethodInfo, type, info.PropertyType);
+                members.Add(memberTypeInfo);
+            }
+
+            foreach (var info in type.GetFields(BindingFlags.Public | BindingFlags.Instance))
+            {
+                var _info = info;
+
+                var memberTypeInfo = ReflectionMember.CreateMember(_info, type, info.FieldType);
+                members.Add(memberTypeInfo);
+            }
+
+            return new TypeInfoReflection
+            {
+                members = members,
+            };
+        }
+    }
+}
---
+++ Elurnity.Serialization/FastReflection/TypeMemberInfoReflection.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Reflection;
+
+namespace Elurnity.Serialization
+{
+    public static class ReflectionMember
+    {
+        internal class ValueTypeMemberReflection<T, U> : ValueTypeMember<T, U> where T : struct
+        {
+            public ValueTypeMemberReflection(MethodInfo getMethod, MethodInfo setMethod)
+            {
+                if (getMethod != null)
+                {
+                    Getter = getMethod.CreateDelegate(typeof(GetterDelegate)) as GetterDelegate;
+                }
+
+                if (setMethod != null)
+                {
+                    Setter = getMethod.CreateDelegate(typeof(SetterDelegate)) as SetterDelegate;
+                }
+            }
+
+            public ValueTypeMemberReflection(Delegate getMethod, Delegate setMethod)
+            {
+                if (getMethod != null)
+                {
+                    Getter = getMethod as GetterDelegate;
+                }
+
+                if (setMethod != null)
+                {
+                    Setter = setMethod as SetterDelegate;
+                }
+            }
+        }
+
+        internal class TypeMemberReflection<T, U> : TypeMember<T, U> where T : class
+        {
+            public TypeMemberReflection(MethodInfo getMethod, MethodInfo setMethod)
+            {
+                if (getMethod != null)
+                {
+                    Getter = getMethod.CreateDelegate(typeof(GetterDelegate)) as GetterDelegate;
+                }
+
+                if (setMethod != null)
+                {
+                    Setter = setMethod.CreateDelegate(typeof(SetterDelegate)) as SetterDelegate;
+                }
+            }
+
+            public TypeMemberReflection(FieldInfo fieldInfo)
+            {
+                var getter = (Func<T, object>)fieldInfo.GetValue;
+
+                // TODO: split and add class restriction to U
+                //var setter = (Action<T, U>)fieldInfo.SetValue;
+
+                Getter = (instance) => (U)getter(instance);
+                Setter = (instance, field) => fieldInfo.SetValue(instance, field);
+            }
+        }
+
+        public static TypeMember CreateMember(MethodInfo getMethod, MethodInfo setMethod, Type instanceType, Type memberType)
+        {
+            var typeArguments = new[] { instanceType, memberType };
+            var type = typeof(TypeMemberReflection<,>).MakeGenericType(typeArguments);
+            return Activator.CreateInstance(type, new[] { getMethod, setMethod }) as TypeMember;
+        }
+
+        public static TypeMember CreateValueMember(MethodInfo getMethod, MethodInfo setMethod, Type instanceType, Type memberType)
+        {
+            var typeArguments = new[] { instanceType, memberType };
+            var type = typeof(ValueTypeMemberReflection<,>).MakeGenericType(typeArguments);
+            return Activator.CreateInstance(type, new[] { getMethod, setMethod }) as TypeMember;
+        }
+
+        public static TypeMember CreateMember(FieldInfo fieldInfo, Type instanceType, Type memberType)
+        {
+            var typeArguments = new[] { instanceType, memberType };
+            var type = typeof(TypeMemberReflection<,>).MakeGenericType(typeArguments);
+            return Activator.CreateInstance(type, fieldInfo) as TypeMember;
+        }
+
+        public static TypeMember CreateValueMember(FieldInfo fieldInfo, Type instanceType, Type memberType)
+        {
+            var typeArguments = new[] { instanceType, memberType };
+            var type = typeof(ValueTypeMemberReflection<,>).MakeGenericType(typeArguments);
+            return Activator.CreateInstance(type, fieldInfo) as TypeMember;
+        }
+    }
+}
