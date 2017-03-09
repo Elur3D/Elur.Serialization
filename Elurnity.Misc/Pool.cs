dccc3e68098212d401814935d73f97c8f45c0add
Jon Ander Hernandez <jonan.h@gmail.com>
2017-03-09 09:11:11 +0200

    Add Elurnity.Misc containing a Singleton, a generic WeakReference and a Pool

---
+++ Elurnity.Misc/Pool.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+
+namespace Elurnity
+{
+    public static class Pool<T> where T : class, new()
+    {
+        private const int PoolSize = 20;
+        private static readonly T[] pool = new T[PoolSize];
+
+        public static T Get()
+        {
+            for (int i = 0; i < pool.Length; i++)
+            {
+                object obj;
+                if ((obj = Interlocked.Exchange(ref pool[i], null)) != null)
+                {
+                    return (T)obj;
+                }
+            }
+            return new T();
+        }
+
+        public static void Release(ref T instance)
+        {
+            int num = 0;
+            while (num < pool.Length && Interlocked.CompareExchange(ref pool[num], instance, null) != null)
+            {
+                num++;
+            }
+            instance = null;
+        }
+    }
+}
---
+++ Elurnity.Misc/Singleton.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Elurnity
+{
+    // Please don't use me. Singletons are bad :-(
+
+    public class Singleton<T> where T : class, new()
+    {
+        private static T _instance;
+        private static object _lock = new object();
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new T();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
